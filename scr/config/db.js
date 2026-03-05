c6a028d63e49aeea67443079601fa2ab3dc61003
subham chakraborty <chakrabortysubham448@gmail.com>
2026-03-05 21:09:22 +0530

    add user model

--- scr/config/db.js
+++ scr/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
  const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, ).then(() => console.log("MongoDB connected"))
+    await mongoose.connect(process.env.MONGODB_URI ).then(() => console.log("MongoDB connected"))
 } catch (error) {
     console.log("Error connecting to MongoDB", error);
     process.exit(1);
---
+++ scr/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        trim:true,
+        lovercase:true,
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Please fill a valid email address"],
+        unique:[true,"Email already exists"]
+    },
+    name:{
+        type:String,
+        required:[true,"Name is required"],
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+        minlength:[6,"Password must be at least 6 characters"],
+        select:false
+    }
+},{
+    timestamps:true
+})
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
+})
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
+const User = mongoose.model("User",userSchema);
+export default User;
