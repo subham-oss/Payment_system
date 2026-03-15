8e0aecb9cc300776338bb56ea7cb41ffe4e9eee1
subham chakraborty <chakrabortysubham448@gmail.com>
2026-03-15 21:28:09 +0530

    add transaction model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    fromAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required:[true,"Transaction must have a from account"],
+        index: true,
+    },
+    toAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required:[true,"Transaction must have a to account"],
+        index: true,
+    },
+    status: {
+        type: String,
+        enum:{ values: ["PENDING", "COMPLETED", "FAILED", "REVISED"],
+                message: "Status must be pending, completed, failed or revised",
+         },
+        default: "PENDING"
+    },
+    amount: {
+        type: Number,
+        required: [true, "Transaction must have an amount"],
+        min: [0, "Amount must be greater than 0"]
+    }
+},{
+    timestamps: true
+})
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
