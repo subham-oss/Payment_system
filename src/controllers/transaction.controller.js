e7292a3f05cb459309a6d4ed48b3a8c13200a062
subham chakraborty <chakrabortysubham448@gmail.com>
2026-03-26 09:51:56 +0530

    add new methods getBalance

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -6,19 +6,17 @@ const createTransaction = async (req, res) => {
   const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
 
   if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
-    return res
-      .status(400)
-      .json({
-        message:
-          "From account, to account, amount and idempotency key are required",
-      });
+    return res.status(400).json({
+      message:
+        "From account, to account, amount and idempotency key are required",
+    });
   }
 
   const fromUserAccount = await Account.findOne({ _id: fromAccount });
   const toUserAccount = await Account.findOne({ _id: toAccount });
 
   if (!fromUserAccount || !toUserAccount) {
-      return res.status(404).json({ message: "Account not found" });
+    return res.status(404).json({ message: "Account not found" });
   }
 
   const isTransactionExist = await Transaction.findOne({
@@ -26,32 +24,42 @@ const createTransaction = async (req, res) => {
   });
 
   if (isTransactionExist) {
-      if(isTransactionExist.status === "COMPLETED"){
-          return res.status(200).json({
-              message: "Transaction already completed",
-              transaction: isTransactionExist,
-          });
-      } 
-      if(isTransactionExist.status === "PENDING"){
-          return res.status(200).json({
-              message: "Transaction already pending",
-          });
-      }
-      if(isTransactionExist.status === "FAILED"){
-          return res.status(500).json({
-              message: "Transaction already failed",
-          });
-      }
-      if(isTransactionExist.status === "REVISED"){
-          return res.status(500).json({
-              message: "Transaction already revised",
-          });
-      }
+    if (isTransactionExist.status === "COMPLETED") {
+      return res.status(200).json({
+        message: "Transaction already completed",
+        transaction: isTransactionExist,
+      });
+    }
+    if (isTransactionExist.status === "PENDING") {
+      return res.status(200).json({
+        message: "Transaction already pending",
+      });
+    }
+    if (isTransactionExist.status === "FAILED") {
+      return res.status(500).json({
+        message: "Transaction already failed",
+      });
+    }
+    if (isTransactionExist.status === "REVISED") {
+      return res.status(500).json({
+        message: "Transaction already revised",
+      });
+    }
   }
 
-  if(fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE"){
-      return res.status(400).json({
-          message: "Both accounts must be active to perform transaction",
-      });
+  if (
+    fromUserAccount.status !== "ACTIVE" ||
+    toUserAccount.status !== "ACTIVE"
+  ) {
+    return res.status(400).json({
+      message: "Both accounts must be active to perform transaction",
+    });
+  }
+  const balance = await fromUserAccount.getBalance();
+  if (balance < amount) {
+    return res.status(400).json({
+      message: `Insufficient balance in from account to perform transaction. Current balance is ${balance}. Please reduce the amount or add funds to the account and try again.`,
+    });
+    
   }
 };
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,30 +1,66 @@
 import mongoose from "mongoose";
+import Ledger from "./ledger.model";
 
-const accountSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: [true, "User is required"],
-        index: true
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+      index: true,
     },
-    status:{
-        type: String,
-        enum:{
-            values: ["ACTIVE","FROZEN", "CLOSED"],
-            message: "Status must be active, frozen or closed",
+    status: {
+      type: String,
+      enum: {
+        values: ["ACTIVE", "FROZEN", "CLOSED"],
+        message: "Status must be active, frozen or closed",
+      },
+      default: "ACTIVE",
+    },
+    currency: {
+      type: String,
+      required: [true, "Currency is required"],
+      default: "INR",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+accountSchema.index({ user: 1, status: 1 });
+
+accountSchema.methods.getBalance = async function () {
+  const balanceData = await Ledger.aggregate([
+    { $match: { account: this._id } },
+    {
+      $group: {
+        _id: null,
+        totalDebit: {
+          $sum: {
+            $cond: [{ $eq: ["$type", "debit"] }, "$amount", 0],
+          },
         },
-        default: "ACTIVE"
+        totalCredit: {
+          $sum: {
+            $cond: [{ $eq: ["$type", "credit"] }, "$amount", 0],
+          },
+        },
+      },
     },
-    currency:{
-        type: String,
-        required: [true, "Currency is required"],
-        default: "INR"
-    }
-},{
-    timestamps: true
-})
+    {
+      $project: {
+        _id: 0,
+        balance: {
+          $subtract: ["$totalCredit", "$totalDebit"],
+        },
+      },
+    },
 
-accountSchema.index({ user: 1 , status:1 });
+  ]);
+    return balanceData.length > 0 ? balanceData[0].balance : 0;
+    
+};
 
 const Account = mongoose.model("Account", accountSchema);
 
